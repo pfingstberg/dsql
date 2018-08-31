296dcf06e97b5dbbcf14d6cd36e165c841dd535d
Pfingstberg <pfingstberg@gmx.net>
2018-08-31 15:09:36 +0200

    Initial .py commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Halo, cześć!"
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
